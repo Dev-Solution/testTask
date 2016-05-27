2c9ceca69410efdba7755cbcfd439e37e5664f3d
Dev-Solution <bogdanov.sergey1991@gmail.com>
2016-05-27 16:49:19 +0300

    Update PrintText.java

--- src/main/java/PrintText.java
+++ src/main/java/PrintText.java
@@ -2,6 +2,7 @@
 public class PrintText {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaa");
+        System.out.println("sssssssssss");
         System.out.println("PringText");
     }
 }
